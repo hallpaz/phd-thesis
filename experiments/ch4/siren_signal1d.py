3329d65e7ed69b95dc446f01df7c801124bce722
Hallison Paz <hallpaz@impa.br>
2024-05-20 16:34:34 -0300

    registered experiments to separate frequencies using S-Net in W&B

--- experiments/ch4/siren_signal1d.py
+++ experiments/ch4/siren_signal1d.py
@@ -12,35 +12,31 @@ from mrnet.datasets.procedural import perlin_noise
 from mrnet.training.utils import load_hyperparameters, get_optim_handler
 from IPython import embed
 
+def crafted_signal(nsamples):
+    X = torch.linspace(-1, 1, nsamples)
+    return (torch.sin(2 * 2 * torch.pi * X) 
+                + torch.sin(5 * 2 * torch.pi * X)
+                + torch.sin(31 * 2 * torch.pi * X)
+                + torch.sin(42 * 2 * torch.pi * X)) / 3
+     
 
-def exp_single_frequency(nsamples = 512):
-    for hidden_features in [100]:#[64, 128]:#, 256, 512, 1024]:
+def exp_crafted_frequencies():
+    for hidden_layers in [0, 1, 2]:
         torch.manual_seed(777)
         #-- hyperparameters in configs --#
         hyper = load_hyperparameters('experiments/ch4/configs/shallow.yml')
-        hyper['nsamples'] = nsamples
-        project_name = hyper.get('project_name', 'phd-thesis')
-        hyper['hidden_features'] = hidden_features
+        nsamples = hyper['nsamples']
+        project_name = hyper['project_name']
+        hyper['hidden_layers'] = hidden_layers
+        hyper['omega_0'] = hyper['omega_0'] * 2 * torch.pi
         
-        X = torch.linspace(-1, 1, nsamples)
-        frequencies = (torch.sin(1 * 2 * torch.pi * X) 
-                       + torch.sin(5 * 2 * torch.pi * X)
-                       + torch.sin(40 * 2 * torch.pi * X)
-                       + torch.sin(50 * 2 * torch.pi * X)) / 3
+        frequencies = crafted_signal(nsamples)
         base_signal = Signal1D(frequencies.view(1, -1), 
                             domain=hyper['domain'],
                             batch_size=hyper['batch_size'])
 
-        train_dataset = create_MR_structure(base_signal,
-                                            hyper['max_stages'],
-                                            hyper['filter'],
-                                            hyper['decimation'],
-                                            hyper['pmode'])
-        test_dataset = create_MR_structure(base_signal,
-                                            hyper['max_stages'],
-                                            hyper['filter'],
-                                            False,
-                                            hyper['pmode'])
+        train_dataset = [base_signal]
+        test_dataset = [base_signal]
 
         # you can substitute this line by your custom handler class
         optim_handler = get_optim_handler(hyper.get('optim_handler', 'regular'))
@@ -51,7 +47,7 @@ def exp_single_frequency(nsamples = 512):
 
         name = os.path.basename(hyper['data_path'])
         training_listener = TrainingListener(project_name,
-                                    f"{name[0:7]}{hyper['model']}{hyper['filter'][0].upper()}",
+                                    f"{name}{hyper['model']}",
                                     hyper,
                                     Path(hyper.get("log_path", "runs")))
 
@@ -63,29 +59,20 @@ def exp_single_frequency(nsamples = 512):
                                             optim_handler=optim_handler)
         mrtrainer.train(hyper['device'])
 
-def exp_noise512():
+def exp_smoothed():
     torch.manual_seed(777)
-    nsamples = 512
     #-- hyperparameters in configs --#
     hyper = load_hyperparameters('experiments/ch4/configs/shallow.yml')
-    hyper['nsamples'] = nsamples
-    project_name = hyper.get('project_name', 'phd-thesis')
+    nsamples = hyper['nsamples']
+    project_name = hyper['project_name']
     
-    noise = perlin_noise(nsamples, 8, 4, 1.4)
-    base_signal = Signal1D(noise.view(1, -1), 
-                           domain=hyper['domain'],
-                           batch_size=hyper['batch_size'])
+    frequencies = crafted_signal(nsamples)
+    base_signal = Signal1D(frequencies.view(1, -1), 
+                        domain=hyper['domain'],
+                        batch_size=hyper['batch_size'])
 
-    train_dataset = create_MR_structure(base_signal,
-                                        hyper['max_stages'],
-                                        hyper['filter'],
-                                        hyper['decimation'],
-                                        hyper['pmode'])
-    test_dataset = create_MR_structure(base_signal,
-                                        hyper['max_stages'],
-                                        hyper['filter'],
-                                        False,
-                                        hyper['pmode'])
+    train_dataset = [base_signal]
+    test_dataset = [base_signal]
 
     # you can substitute this line by your custom handler class
     optim_handler = get_optim_handler(hyper.get('optim_handler', 'regular'))
@@ -94,9 +81,9 @@ def exp_noise512():
     print("Model: ", type(mrmodel))
     print(mrmodel)
 
-    name = os.path.basename(hyper['data_path'])
+    name = "smoothed"
     training_listener = TrainingListener(project_name,
-                                f"{name[0:7]}{hyper['model']}{hyper['filter'][0].upper()}",
+                                f"{name}{hyper['model']}",
                                 hyper,
                                 Path(hyper.get("log_path", "runs")))
 
@@ -109,6 +96,92 @@ def exp_noise512():
     mrtrainer.train(hyper['device'])
 
 
+def exp_high_frequencies_only():
+    torch.manual_seed(777)
+    #-- hyperparameters in configs --#
+    hyper = load_hyperparameters('experiments/ch4/configs/shallow.yml')
+    nsamples = hyper['nsamples']
+    project_name = hyper['project_name']
+    
+    high_tone = 45 * 2 * torch.pi
+    low_tone = 25 * 2 * torch.pi
+    hyper['omega_0'] = high_tone
+    
+    frequencies = crafted_signal(nsamples)
+    base_signal = Signal1D(frequencies.view(1, -1), 
+                        domain=hyper['domain'],
+                        batch_size=hyper['batch_size'])
+
+    train_dataset = [base_signal]
+    test_dataset = [base_signal]
+
+    # you can substitute this line by your custom handler class
+    optim_handler = get_optim_handler(hyper.get('optim_handler', 'regular'))
+    
+    mrmodel = MRFactory.from_dict(hyper)
+    print("Model: ", type(mrmodel))
+    with torch.no_grad():
+        layer_width = len(mrmodel.stages[0].first_layer.linear.weight)
+        print(layer_width)
+        mrmodel.stages[0].first_layer.linear.weight[:layer_width//2].uniform_(-1, -low_tone/high_tone)
+        mrmodel.stages[0].first_layer.linear.weight[layer_width//2:].uniform_(low_tone/high_tone, 1)
+    print(mrmodel)
+
+    name = os.path.basename(hyper['data_path'])
+    training_listener = TrainingListener(project_name,
+                                f"{name}{hyper['model']}l{low_tone:.1f}h{high_tone:.1f}",
+                                hyper,
+                                Path(hyper.get("log_path", "runs")))
+
+    mrtrainer = MRTrainer.init_from_dict(mrmodel,
+                                        train_dataset,
+                                        test_dataset,
+                                        training_listener,
+                                        hyper,
+                                        optim_handler=optim_handler)
+    mrtrainer.train(hyper['device'])
+
+def exp_all_frequencies():
+    torch.manual_seed(777)
+    #-- hyperparameters in configs --#
+    hyper = load_hyperparameters('experiments/ch4/configs/shallow.yml')
+    nsamples = hyper['nsamples']
+    project_name = hyper['project_name']
+    
+    hyper['omega_0'] = 45 * 2 * torch.pi
+    hyper['hidden_features'] = 128
+    
+    frequencies = crafted_signal(nsamples)
+    base_signal = Signal1D(frequencies.view(1, -1), 
+                        domain=hyper['domain'],
+                        batch_size=hyper['batch_size'])
+
+    train_dataset = [base_signal]
+    test_dataset = [base_signal]
+
+    # you can substitute this line by your custom handler class
+    optim_handler = get_optim_handler(hyper.get('optim_handler', 'regular'))
+    
+    mrmodel = MRFactory.from_dict(hyper)
+    print("Model: ", type(mrmodel))
+    print(mrmodel)
+
+    name = os.path.basename(hyper['data_path'])
+    training_listener = TrainingListener(project_name,
+                                f"{name}{hyper['model']}",
+                                hyper,
+                                Path(hyper.get("log_path", "runs")))
+
+    mrtrainer = MRTrainer.init_from_dict(mrmodel,
+                                        train_dataset,
+                                        test_dataset,
+                                        training_listener,
+                                        hyper,
+                                        optim_handler=optim_handler)
+    mrtrainer.train(hyper['device'])
+
 if __name__ == '__main__':
-    # exp_noise512()
-    exp_single_frequency()
+    exp_smoothed()
+    exp_crafted_frequencies()
+    exp_high_frequencies_only()
+    exp_all_frequencies()    
