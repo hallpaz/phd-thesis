eb57d264ba046bad38192c707a9b391bf1fa5002
Hallison Paz <hallpaz@impa.br>
2024-09-09 11:37:13 -0300

    before structural changes to reuse code

--- experiments/ch4/siren_signal1d.py
+++ experiments/ch4/siren_signal1d.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 import torch
+import inspect
 
 from mrnet.training.trainer import MRTrainer
 from mrnet.datasets.signals import Signal1D
@@ -12,22 +13,28 @@ from mrnet.datasets.procedural import perlin_noise
 from mrnet.training.utils import load_hyperparameters, get_optim_handler
 from IPython import embed
 
+
 def crafted_signal(nsamples):
     X = torch.linspace(-1, 1, nsamples)
     return (torch.sin(2 * 2 * torch.pi * X) 
                 + torch.sin(5 * 2 * torch.pi * X)
                 + torch.sin(31 * 2 * torch.pi * X)
                 + torch.sin(42 * 2 * torch.pi * X)) / 3
+
+def training_pipeline():
+    pass
      
 
 def exp_crafted_frequencies():
-    for hidden_layers in [0, 1, 2]:
+    for hidden_layers in [1]:#[0, 1, 2]:
         torch.manual_seed(777)
         #-- hyperparameters in configs --#
         hyper = load_hyperparameters('experiments/ch4/configs/shallow.yml')
         nsamples = hyper['nsamples']
         project_name = hyper['project_name']
+        name = inspect.currentframe().f_code.co_name.replace('exp_', '')
         hyper['hidden_layers'] = hidden_layers
+        # multiples w0 by 2pi
         hyper['omega_0'] = hyper['omega_0'] * 2 * torch.pi
         
         frequencies = crafted_signal(nsamples)
@@ -45,7 +52,7 @@ def exp_crafted_frequencies():
         print("Model: ", type(mrmodel))
         print(mrmodel)
 
-        name = os.path.basename(hyper['data_path'])
+        
         training_listener = TrainingListener(project_name,
                                     f"{name}{hyper['model']}",
                                     hyper,
@@ -81,7 +88,7 @@ def exp_smoothed():
     print("Model: ", type(mrmodel))
     print(mrmodel)
 
-    name = "smoothed"
+    name = inspect.currentframe().f_code.co_name.replace('exp_', '')
     training_listener = TrainingListener(project_name,
                                 f"{name}{hyper['model']}",
                                 hyper,
@@ -129,9 +136,11 @@ def exp_high_frequencies_only():
                 mrmodel.stages[0].first_layer.linear.weight[layer_width//2:].uniform_(low_tone/high_tone, 1)
             print(mrmodel)
 
-            name = os.path.basename(hyper['data_path'])
+            name = inspect.currentframe().f_code.co_name.replace('exp_', '')
+            low_tone = int(low_tone / (2 * torch.pi))
+            high_tone = int(high_tone / (2 * torch.pi))
             training_listener = TrainingListener(project_name,
-                                        f"{name}{hyper['model']}l{low_tone:.1f}h{high_tone:.1f}",
+                                        f"{name}{hyper['model']}l{low_tone}h{high_tone}",
                                         hyper,
                                         Path(hyper.get("log_path", "runs")))
 
@@ -149,41 +158,83 @@ def exp_all_frequencies():
     hyper = load_hyperparameters('experiments/ch4/configs/shallow.yml')
     nsamples = hyper['nsamples']
     project_name = hyper['project_name']
-    
+
     hyper['omega_0'] = 45 * 2 * torch.pi
-    hyper['hidden_features'] = 128
-    
-    frequencies = crafted_signal(nsamples)
-    base_signal = Signal1D(frequencies.view(1, -1), 
-                        domain=hyper['domain'],
-                        batch_size=hyper['batch_size'])
+    for hf in [64, 128, 256]:
+        hyper['hidden_features'] = hf
+        
+        frequencies = crafted_signal(nsamples)
+        base_signal = Signal1D(frequencies.view(1, -1), 
+                            domain=hyper['domain'],
+                            batch_size=hyper['batch_size'])
 
-    train_dataset = [base_signal]
-    test_dataset = [base_signal]
+        train_dataset = [base_signal]
+        test_dataset = [base_signal]
 
-    # you can substitute this line by your custom handler class
-    optim_handler = get_optim_handler(hyper.get('optim_handler', 'regular'))
-    
-    mrmodel = MRFactory.from_dict(hyper)
-    print("Model: ", type(mrmodel))
-    print(mrmodel)
+        # you can substitute this line by your custom handler class
+        optim_handler = get_optim_handler(hyper.get('optim_handler', 'regular'))
+        
+        mrmodel = MRFactory.from_dict(hyper)
+        print("Model: ", type(mrmodel))
+        print(mrmodel)
 
-    name = os.path.basename(hyper['data_path'])
-    training_listener = TrainingListener(project_name,
-                                f"{name}{hyper['model']}",
-                                hyper,
-                                Path(hyper.get("log_path", "runs")))
+        name = inspect.currentframe().f_code.co_name.replace('exp_', '')
+        training_listener = TrainingListener(project_name,
+                                    f"{name}{hyper['model']}",
+                                    hyper,
+                                    Path(hyper.get("log_path", "runs")))
 
-    mrtrainer = MRTrainer.init_from_dict(mrmodel,
-                                        train_dataset,
-                                        test_dataset,
-                                        training_listener,
-                                        hyper,
-                                        optim_handler=optim_handler)
-    mrtrainer.train(hyper['device'])
+        mrtrainer = MRTrainer.init_from_dict(mrmodel,
+                                            train_dataset,
+                                            test_dataset,
+                                            training_listener,
+                                            hyper,
+                                            optim_handler=optim_handler)
+        mrtrainer.train(hyper['device'])
+
+def exp_hidden_layers():
+    torch.manual_seed(777)
+    #-- hyperparameters in configs --#
+    hyper = load_hyperparameters('experiments/ch4/configs/hiddenlayer.yml')
+    nsamples = hyper['nsamples']
+    project_name = hyper['project_name']
+
+    hyper['omega_0'] = 45 * 2 * torch.pi
+    for hf in [16, 24, 32, 64]:
+        hyper['hidden_features'] = hf
+        
+        frequencies = crafted_signal(nsamples)
+        base_signal = Signal1D(frequencies.view(1, -1), 
+                            domain=hyper['domain'],
+                            batch_size=hyper['batch_size'])
+
+        train_dataset = [base_signal]
+        test_dataset = [base_signal]
+
+        # you can substitute this line by your custom handler class
+        optim_handler = get_optim_handler(hyper.get('optim_handler', 'regular'))
+        
+        mrmodel = MRFactory.from_dict(hyper)
+        print("Model: ", type(mrmodel))
+        print(mrmodel)
+
+        name = inspect.currentframe().f_code.co_name.replace('exp_', '')
+        training_listener = TrainingListener(project_name,
+                                    f"{name}{hyper['model']}",
+                                    hyper,
+                                    Path(hyper.get("log_path", "runs")))
+
+        mrtrainer = MRTrainer.init_from_dict(mrmodel,
+                                            train_dataset,
+                                            test_dataset,
+                                            training_listener,
+                                            hyper,
+                                            optim_handler=optim_handler)
+        mrtrainer.train(hyper['device'])
 
 if __name__ == '__main__':
     exp_smoothed()
-    # exp_crafted_frequencies()
-    # exp_high_frequencies_only()
-    # exp_all_frequencies()    
+    exp_crafted_frequencies()
+    exp_high_frequencies_only()
+    exp_all_frequencies()
+    exp_hidden_layers()
